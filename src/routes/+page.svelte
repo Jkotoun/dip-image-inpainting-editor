5bf0d00f99bdb8dd5f1d5ee5f929f7f72f50ea25
Jkotoun <jkotoun58@gmail.com>
2023-11-18 16:11:09 +0100

    Add models and backend + models loading flow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,6 @@
 <script>
-    let a = 10;
-    $: b = 2 * a
 </script>
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>{a}</p>
-<p>{b}</p>
-
-<button on:click={() => a++}>Increment</button>
 
 
 <a href="/model_demo">model demo</a>
--- src/routes/model_demo/+page.svelte
+++ src/routes/model_demo/+page.svelte
@@ -1,87 +1,96 @@
-<script>
-// @ts-nocheck
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import * as tf from '@tensorflow/tfjs';
+  //TODO move loadBackend and loadModels to some lib file
+	async function loadBackend() {
+		await tf.ready();
+		try {
+			await import('@tensorflow/tfjs-backend-webgpu');
+			await tf.setBackend('webgpu');
+			console.log(tf.env().getFlags());
+		} catch (e) {
+			try {
+				await tf.setBackend('webgl');
+			} catch (e) {
+				console.error('could not load backend:', e);
+				throw e;
+			}
+		}
+		await loadModels();
+	}
 
-    import { onMount } from 'svelte';
-    import * as tf from '@tensorflow/tfjs';
-    import '@tensorflow/tfjs-backend-webgl'; // Load WebGL backend
-    import '@tensorflow/tfjs-converter'; // Load model converter
-    import { IMAGENET_CLASSES } from './imagenet_classes';
-    let model;
-    let imageFile;
-    let predictedClass = 0;
+  async function loadModels() {
+		try {
+			const imgEncoderPromise = tf.loadGraphModel('/mobile_enc_tfjs/model.json');
+			const decoderPromise = tf.loadGraphModel('/base_dec_tfjs/model.json');
+			const wavepaintPromise = tf.loadGraphModel('/wavepaint_tfjs_256/model.json');
+			const [imgEncoderValue, decoderValue, wavepaintValue] = await Promise.all([
+				imgEncoderPromise,
+				decoderPromise,
+				wavepaintPromise
+			]);
+			wavepaint = wavepaintValue;
+			SAMDecoder = decoderValue;
+			SAMEncoder = imgEncoderValue;
+			isLoading = false; // Models loaded successfully
+		} catch (error) {
+			console.error('Error loading the model:', error);
+			isLoading = false; // Set loading state to false even on error
+		}
+	}
 
+	async function onLoad() {
+		try {
+			await loadBackend();
+			console.log(tf.getBackend(), 'backend loaded'); //should be webgpu
+		} catch (e) {
+      isLoading = false;
+			console.error('could not load backend:', e);
+		}
+		try {
+			await loadModels();
+			console.log('models loaded');
+			console.log('enc', SAMEncoder);
+			console.log('dec', SAMDecoder);
+			console.log('wave', wavepaint);
+		} catch (e) {
+      isLoading = false;
+			console.error('could not load models:', e);
+		}
+	}
 
-    let isLoading = true;
-    const MOBILENET_MODEL_PATH =
-    'https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v2_100_224/classification/3/default/1';
+	let SAMEncoder: tf.GraphModel<string | tf.io.IOHandler>;
+	let SAMDecoder: tf.GraphModel<string | tf.io.IOHandler>;
+	let wavepaint: tf.GraphModel<string | tf.io.IOHandler>;
+	let isLoading = true;
+	
+	onMount(onLoad); // Load TFJS backend and models
+</script>
 
-    const IMAGE_SIZE = 224;
-    
+{#if isLoading}
+	<p>Loading model...</p>
+{:else if SAMEncoder && SAMDecoder && wavepaint}
+	<h1>AI Object remover</h1>
+	<h2>Image Encoder</h2>
+	<ul>
+		{#each SAMEncoder.inputs as input}
+			<li>{input.name}, [{input.shape}], {input.dtype}</li>
+		{/each}
+	</ul>
+	<h2>Promt Decoder</h2>
+	<ul>
+		{#each SAMDecoder.inputs as input}
+			<li>{input.name}, [{input.shape}], {input.dtype}</li>
+		{/each}
+	</ul>
+	<h2>Wavepaint</h2>
+	<ul>
+		{#each wavepaint.inputs as input}
+			<li>{input.name}, [{input.shape}], {input.dtype}</li>
+		{/each}
+	</ul>
+{/if}
 
-    async function loadModel() {
-        try {
-        model = await tf.loadGraphModel(MOBILENET_MODEL_PATH, { fromTFHub: true })
-
-        console.log("model_loaded")
-        isLoading = false; // Model loaded successfully
-      } catch (error) {
-        console.error('Error loading the model:', error);
-        isLoading = false; // Set loading state to false even on error
-      }
-    }
-  
-    async function classifyImage() {
-    console.log('Image loaded');
-
-    const img = new Image();
-    img.src = URL.createObjectURL(imageFile);
-    await img.decode();
-
-    const imgTensor = tf.tidy(() => {
-        // tf.browser.fromPixels() returns a Tensor from an image element.
-        const tempImgTensor = tf.browser.fromPixels(img).toFloat();
-
-        // Normalize the image from [0, 255] to [-1, 1].
-        const offset = tf.scalar(127.5);
-        const normalized = tempImgTensor.sub(offset).div(offset);
-
-        return normalized;
-    });
-
-    // Resize the image if its dimensions are not equal to IMAGE_SIZE.
-    const resizedImgTensor = tf.image.resizeBilinear(imgTensor, [IMAGE_SIZE, IMAGE_SIZE]);
-
-    const batched = resizedImgTensor.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
-
-    // Make a prediction through the model.
-    const pred = model.predict(batched);
-
-    // Get top class.
-    const topClass = pred.argMax(1).dataSync()[0];
-
-    // Clean up tensors.
-    imgTensor.dispose();
-    resizedImgTensor.dispose();
-
-    predictedClass = topClass;
-}
-  
-    onMount(loadModel); // Load the model when the component mounts
-  </script>
-  
-  {#if isLoading}
-    <p>Loading model...</p>
-  {:else}
-  <h1>Image Classification App</h1>
-  
-  <input type="file" accept="image/*" on:change={(e) => (imageFile = e.target.files[0])} />
-  <button on:click={classifyImage}>Classify</button>
-  {#if predictedClass !== ''}
-  <p>{IMAGENET_CLASSES[predictedClass]}</p>
-  
-  {/if}
-  {/if}
-  <style>
-    /* Add your styles here */
-  </style>
-  
+<style>
+	/* Add your styles here */
+</style>
