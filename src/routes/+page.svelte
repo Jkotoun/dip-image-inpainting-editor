bcd77edabd33698555346ff5129e46e234561a0c
Jkotoun <jkotoun58@gmail.com>
2024-04-20 21:08:51 +0200

    Add comparison and comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,7 @@
 	import { goto } from '$app/navigation';
 	import { uploadedImgBase64, uploadedImgFileName, uploadedImgTargetRes } from '../stores/imgStore';
 	import { mainWorker } from '../stores/workerStore';
-	import { browser } from '$app/environment';
-
+	import { CompareImage } from 'svelte-compare-image';
 	import {
 		AppShell,
 		FileDropzone,
@@ -16,8 +15,9 @@
 	import { MESSAGE_TYPES } from '../workers/messageTypes';
 	import Navbar from './../components/Navbar.svelte';
 	import EditStepCard from '../components/EditStepCard.svelte';
-	let gExamplesContainerElem: HTMLDivElement;
 
+	//example images carousel scroll functions
+	let gExamplesContainerElem: HTMLDivElement;
 	function multiColumnLeft(): void {
 		let x = gExamplesContainerElem.scrollWidth;
 		if (gExamplesContainerElem.scrollLeft !== 0)
@@ -31,11 +31,13 @@
 		if (
 			gExamplesContainerElem.scrollLeft <
 			gExamplesContainerElem.scrollWidth - gExamplesContainerElem.clientWidth - 1
-		)
+		) {
 			x = gExamplesContainerElem.scrollLeft + gExamplesContainerElem.clientWidth;
+		}
 		gExamplesContainerElem.scroll(x, 0);
 	}
-	//high res resize modal
+
+	//high resolution resize modal
 	const modalStore = getModalStore();
 	const longsideResResizeThreshold = 2000;
 
@@ -81,41 +83,40 @@
 		`${base}/example_photos/example_people_sunset.jpg`,
 		`${base}/example_photos/lukas_snih.jpg`,
 		`${base}/example_photos/20230808_181343.jpg`,
-		`${base}/example_photos/example_sunset2.jpg`,
+		`${base}/example_photos/example_sunset2.jpg`
 	];
 	// `${base}/example_photos/free-photo-of-ryma-studeny-snih-drevo.jpeg`,
 	// `${base}/example_photos/mountain-5649827_1280.jpg`,
 	// `${base}/example_photos/1920resized_street.jpg`
-	
+
+	//image resolution from its url 
 	async function getImageResolution(url: string): Promise<{ width: number; height: number }> {
-		const img = document.createElement('img');
+		const img = new Image();
 		const promise: Promise<{ width: number; height: number }> = new Promise((resolve, reject) => {
 			img.onload = () => {
 				const width = img.naturalWidth;
 				const height = img.naturalHeight;
 				resolve({ width, height });
 			};
-
 			img.onerror = reject;
 		});
 
 		img.src = url;
 		return promise;
 	}
-	//https://stackoverflow.com/questions/22172604/convert-image-from-url-to-base64
-	async function getImgDataFromUrl(
-		url: string
-	): Promise<{
+
+	//get image base64data, name and resolution from its url - used for example images
+	async function getImgDataFromUrl(url: string): Promise<{
 		base64data: string;
 		imageName: string;
 		resolution: { width: number; height: number };
 	}> {
+		//img name
 		const urlParts = url.split('/');
 		const imageName: string = urlParts[urlParts.length - 1];
 
 		const data = await fetch(url);
 		const blob = await data.blob();
-
 		const img = new Image();
 		img.src = URL.createObjectURL(blob);
 		const promise: any = new Promise((resolve, reject) => {
@@ -123,18 +124,11 @@
 			reader.readAsDataURL(blob);
 			reader.onloadend = () => {
 				const base64data: string = reader.result as string;
-				img.onload = () => {
-					const resolution: { width: number; height: number } = {
-						width: img.width,
-						height: img.height
-					};
-					resolve({ base64data: base64data, imageName: imageName, resolution });
+				const resolution: { width: number; height: number } = {
+					width: img.width,
+					height: img.height
 				};
-
-				// If there's an error loading the image, reject the promise
-				img.onerror = () => {
-					reject('Error loading image');
-				};
+				resolve({ base64data: base64data, imageName: imageName, resolution });
 			};
 			reader.onerror = reject;
 		});
@@ -155,10 +149,11 @@
 		reader.onload = async (e) => {
 			// Store the uploaded image data in the store
 			uploadedImgBase64.set(e.target?.result as string);
-
 			const { width, height } = await getImageResolution(e.target?.result as string);
 			uploadedImgTargetRes.set({ width, height });
+			//check if should show resize modal or redirect to editor
 			if (width > longsideResResizeThreshold || height > longsideResResizeThreshold) {
+				//if show, calc new resolution according to longer side pixels limit
 				let aspectRatio = width / height;
 				let newWidth =
 					aspectRatio > 1
@@ -221,8 +216,6 @@
 	</svelte:fragment>
 
 	<div>
-		<!-- <button on:click={() => }>asda</button> -->
-
 		<div class="py-4 2xl:px-24 px-8">
 			<h1 class="h1 pt-8 font-bold">Remove objects from images with powerful AI tools</h1>
 			<h3 class="h3 pt-4">
@@ -231,38 +224,52 @@
 			</h3>
 			<div class="md:pt-16 pt-4 flex md:flex-row flex-col-reverse gap-4">
 				<div class="flex-1">
-					<img src="{base}/img/before_after_example.png" alt="before_after_exampler" />
+					<CompareImage
+						imageLeftSrc="/before.png"
+						imageLeftAlt="left"
+						imageRightSrc="/after.png"
+						imageRightAlt="right"
+						--handle-size="1.625rem"
+					/>
 				</div>
-				<div class="flex-1  ">
-					
-						<FileDropzone name="files" padding="2xl:py-16 xl:py-12 lg:py-8 md:py-2 py-8 " accept="image/*" on:change={(e) => handleImageUpload(e)}>
-							<svelte:fragment slot="lead">
-								<div class="flex justify-center">
-									<FileUp size={64} />
-								</div>
-							</svelte:fragment>
-							<svelte:fragment slot="message"
-								><span class="font-semibold">Upload an image or drag and drop</span
-								></svelte:fragment
-							>
-						</FileDropzone>
-					
+				<div class="flex-1">
+					<FileDropzone
+						name="files"
+						padding="2xl:py-28 xl:py-20 lg:py-8 md:py-2 py-8 "
+						accept="image/*"
+						on:change={(e) => handleImageUpload(e)}
+					>
+						<svelte:fragment slot="lead">
+							<div class="flex justify-center">
+								<FileUp size={64} />
+							</div>
+						</svelte:fragment>
+						<svelte:fragment slot="message"
+							><span class="font-semibold">Upload an image or drag and drop</span></svelte:fragment
+						>
+					</FileDropzone>
+
 					<h4 class="h4 font-semibold pt-4 pb-2 text-center">Or try with an example</h4>
-					<div class="grid grid-cols-[auto_1fr_auto] gap-4 items-center  pt-4">
+					<div class="grid grid-cols-[auto_1fr_auto] gap-4 items-center pt-4">
 						<!-- Button: Left -->
-						<button type="button" class="btn-icon btn-icon-sm variant-filled" on:click={multiColumnLeft}>
-							<ArrowLeft size={20}/>
+						<button
+							type="button"
+							class="btn-icon btn-icon-sm variant-filled"
+							on:click={multiColumnLeft}
+						>
+							<ArrowLeft size={20} />
 						</button>
 						<!-- Carousel -->
 						<div
 							bind:this={gExamplesContainerElem}
-							class="snap-x snap-mandatory  max-h-[125px]   scroll-smooth flex gap-5 pb-2 overflow-hidden "
+							class="snap-x snap-mandatory max-h-[130px] scroll-smooth flex gap-5 pb-2 overflow-hidden"
 						>
 							{#each exampleImages as imgpath}
 								<button
 									type="button"
 									class="shrink-0 w-[28%] snap-start"
 									on:click={async (e) => {
+										//set image data to global store and redirect to editor
 										const { base64data, imageName, resolution } = await getImgDataFromUrl(imgpath);
 										uploadedImgBase64.set(base64data);
 										uploadedImgFileName.set(imageName);
@@ -270,12 +277,20 @@
 										goto(`${base}/editor`);
 									}}
 								>
-									<img class="cursor-pointer  object-cover object-center" src={imgpath} alt="example_image" />
+									<img
+										class="cursor-pointer object-cover object-center"
+										src={imgpath}
+										alt="example_image"
+									/>
 								</button>
 							{/each}
 						</div>
 						<!-- Button-Right -->
-						<button type="button" class="btn-icon btn-icon-sm variant-filled" on:click={multiColumnRight}>
+						<button
+							type="button"
+							class="btn-icon btn-icon-sm variant-filled"
+							on:click={multiColumnRight}
+						>
 							<ArrowRight size={20} />
 						</button>
 					</div>
@@ -291,10 +306,3 @@
 		</div>
 	</div>
 </AppShell>
-
-
-<style>
-	.dropzone{
-		height: 100% !important
-	} 
-</style>
